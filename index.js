1c29543a69c0ccc8cf5f7c32fe9dfea83050736d
TrashGods <walkingtrash123@gmail.com>
2022-02-06 14:43:03 +0200

    retesting this

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 var test = "test";
 console.log(process.version);// retest
+
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log('Express server listening on port', port)
+});

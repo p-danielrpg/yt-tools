e985ea0cd5fba319de69a952fde1ced429a55273
TrashGods <walkingtrash123@gmail.com>
2022-02-06 15:57:15 +0200

    added yt downloader prime

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const bodyParser = require("body-parser");
+const ytdl = require("youtube-dl.js");
 const app = express();
  
+app.use(express.static(__dirname));
+
+// get our app to use body parser 
+app.use(bodyParser.urlencoded({ extended: true }))
+
 app.get('/', (req, res) => {
 //   res
 //     .status(200)
@@ -8,7 +15,31 @@ app.get('/', (req, res) => {
 //     .end();
     res.sendFile(__dirname + "/index.html");
 });
+
+app.post("/", (req, res) => {
+  var subName = req.body.email;
+ res.send("Hello " + subName + ", Thank you for subcribing. ");
+});
+
+
+
+// ------------------
+// Get youtube info
+let url = "https://www.youtube.com/watch?v=xEbNMPCYlOM",
+    filename = `${new Date().getTime()}.%(ext)s`,
+    args = ["-o", filename, "-x", "--audio-format=mp3", "--restrict-filenames", "--external-downloader=ffmpeg", "--audio-quality=96k"];
  
+ytdl(url, args)
+  .then(data => {
+    console.log(data);
+  })
+  .catch(err => {
+    console.error(err);
+});
+	
+
+
+
 // Start the server
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {

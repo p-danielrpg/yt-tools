f18cabec0de8b94ff2d2c762d16be472c7afa754
TrashGods <walkingtrash123@gmail.com>
2022-02-07 13:34:38 +0200

    finally made this work

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const ytdl = require('ytdl-core');
 const { createServer } = require("http");
 //var io = require('socket.io').listen(appHttp);
 const { Server } = require("socket.io");
-const { callbackify } = require('util');
 const app = express();
 const httpServer = createServer(app);  
 const io = new Server(httpServer, {
@@ -41,7 +40,28 @@ io.on("connection", (socket) => {
         //socket.emit("sendText", "testt test");
     })
 
+  
     
+    socket.on("update item", (arg1, arg2, callback) => {
+      console.log(arg1); // 1
+      console.log(arg2); // { name: "updated" }
+      callback({
+        status: "ok"
+      });
+    });   
+
+    socket.on("videoQue", (arg) => {
+      let download = ytdl(arg).pipe(fs.createWriteStream('video2.mp3'));
+      let videoInfo = ytdl.getInfo(arg);
+      videoInfo.then(function(result) {
+        return result.videoDetails.title;
+        // videoTitle = result.videoDetails.title;
+      }).then(function(neww) {
+        videoTitle = neww;
+        socket.emit("sendText", videoTitle)
+      });
+   })
+
     // function sendBack(argu) {
     //     console.log("it was sent back");
     //     io.sockets.emit("sendText", argu);
@@ -50,55 +70,61 @@ io.on("connection", (socket) => {
 });
 
 
+io.on("disconnect", function () {
+  console.log("client disconnected");
+})
 
-
-httpServer.listen(PORT);
 
 // Get the request to render the INDEX.HTML
 app.get('/', (req, res) => {
-//   res
-//     .status(200)
-//     .send('Hello server is running')
-//     .end();
-    res.sendFile(__dirname + "/index.html");
+  //   res
+  //     .status(200)
+  //     .send('Hello server is running')
+  //     .end();
+  res.render(__dirname + "/index");
 });
+
+
 
 app.post("/", (req, res) => {
   var subName = req.body.email;
-//   res.set('content-disposition', `attachment; filename="${ req.body.subName }"`);
+  //   res.set('content-disposition', `attachment; filename="${ req.body.subName }"`);
   // download youtube video
   let download = ytdl(subName).pipe(fs.createWriteStream('video2.mp3'));
   let videoInfo = ytdl.getInfo(subName);
-
+  
   download.on("finish", ()=> {
     let videoTitle = 0;
     videoInfo.then(function(result) {
-        return result.videoDetails.title;
-       // videoTitle = result.videoDetails.title;
+      return result.videoDetails.title;
+      // videoTitle = result.videoDetails.title;
     }).then(function(neww) {
-        videoTitle = neww;
-        res.download('video2.mp3', String(videoTitle)+".mp3");
-        
-
+      videoTitle = neww;
+      // res.render(__dirname + "/index", {
+      //   text
+      // });
+      res.download('video2.mp3', String(videoTitle)+".mp3");
+    
     })
-
+    
     // res.writeHead(200, {
-    //     "Content-Type": "audio/mpeg",
-    //     "Content-Disposition" : "attachment; filename=" + 
-    //     "audioNow.mp3"   });
-    //"Content-Type": "audio/mpeg";
-    console.log("finished stream");
-  })
-
-  //console.log(download);
- //res.send("Hello " + subName + ", Thank you for subcribing. ");
-});
-
-
-
-
-
-
+      //     "Content-Type": "audio/mpeg",
+      //     "Content-Disposition" : "attachment; filename=" + 
+      //     "audioNow.mp3"   });
+      //"Content-Type": "audio/mpeg";
+      console.log("finished stream");
+    })
+    
+    //console.log(download);
+    //res.send("Hello " + subName + ", Thank you for subcribing. ");
+  });
+  
+  
+  
+  
+  
+  
+  httpServer.listen(PORT);
 
 // app.listen(PORT, () => {
 //   console.log(`App listening on port ${PORT}`);
